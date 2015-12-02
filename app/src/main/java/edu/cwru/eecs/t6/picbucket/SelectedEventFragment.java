d0919e18edab13f8e43b60615e730df35022d139
Walter Huang <zxh108@case.edu>
2015-12-02 05:34:24 -0500

    Selected event fragment

---
+++ app/src/main/java/edu/cwru/eecs/t6/picbucket/SelectedEventFragment.java
@@ -0,0 +1,61 @@
+package edu.cwru.eecs.t6.picbucket;
+
+import android.app.Fragment;
+import android.content.ContentUris;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.CalendarContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SelectedEventFragment extends Fragment {
+
+    TextView startTimeView;
+    TextView endTimeView;
+    TextView eventTitle;
+    Button editEvent;
+    String eventIDString;
+    String eventLocation;
+
+    static final String START_TIME = "start";
+    static final String END_TIME = "end";
+    static final String EVENT_TITLE = "title";
+    static final String EVENT_ID = "id";
+    static final String LOCATION = "location";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View eventView = inflater.inflate(R.layout.fragment_timeline, container, false);
+        startTimeView = (TextView)eventView.findViewById(R.id.startTime);
+        endTimeView = (TextView)eventView.findViewById(R.id.endTime);
+        eventTitle = (TextView)eventView.findViewById(R.id.eventTitle);
+
+        //edit event button
+        editEvent = (Button)eventView.findViewById(R.id.editEvent);
+        editEvent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long eventID = Long.parseLong(eventIDString);
+                Uri uri = ContentUris.withAppendedId(CalendarContract.Events.CONTENT_URI, eventID);
+                Intent intent = new Intent(Intent.ACTION_EDIT)
+                        .setData(uri);
+                startActivity(intent);
+            }
+        });
+
+        return eventView;
+    }
+
+    public void setArgument(Bundle argument){
+        startTimeView.setText((String)argument.get(START_TIME));
+        endTimeView.setText((String)argument.get(END_TIME));
+        eventTitle.setText((String)argument.get(EVENT_TITLE));
+        eventIDString = ((String)argument.get(EVENT_ID));
+        eventLocation = ((String)argument.get(LOCATION));
+    }
+}
